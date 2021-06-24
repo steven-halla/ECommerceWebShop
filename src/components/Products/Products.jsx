d1d42e41c53be79a4aba278ca5054d30c7a3b32e
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-06-23 18:58:31 -0700

    "second push"

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
-import Grid from @material-ui/core;
+import { Grid } from '@material-ui/core';
+
+import Product from './Product/Product';
+import useStyles from './styles';
+
 
 const products = [
-  {id:1, name: 'Shoes', description: 'Running shoes.', price: '$5'},
-  {id:2, name: 'Macbook', description: 'Apple macbook.', price: '$10'},
+  {id:1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://i.insider.com/5e38419b5bc79c4c7d4e1192?width=906&format=jpeg'},
+  {id:2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: 'https://www.macworld.co.uk/cmsdata/features/3685961/macbook_air_2019_review_9_thumb800.jpg'},
 ];
 
 const Products = () => {
-  <main>
-    <Grid container justify="center" spacing={4}>
-      {products.map((products) => (
-        <Grid item key={prodcut.id} xs={12} sm={6} md={4} lg={3}>
-          <Product product={product} />
-        </Grid>
-      ))}
-    </Grid>
-  </main>
+  const classes = useStyles();
+
+  return(
+
+    <main className={classes.content}>
+      <div className={classes.toolbar}/>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
 }
 
 export default Products;
