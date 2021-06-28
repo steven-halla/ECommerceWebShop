1e3eeaec13cf9698ccfd77ea9ea23c0138bfe900
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-06-28 11:25:56 -0700

    "CP2"

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -19,6 +19,7 @@ export const AddressForm = ({ checkoutToken }) => {
     const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
     console.log(countries);
     setShippingCountries(countries);
+    setShippingCountry(Object.keys(countries)[0]);
   }
 
   useEffect(() => {
@@ -39,14 +40,15 @@ export const AddressForm = ({ checkoutToken }) => {
             <FormInput required name='email' label="Email" />
             <FormInput required name='city' label="City" />
             <FormInput required name='zip' label="ZIP / Postal code" />
-          {/*  <Grid item xs={12} sm={6}>*/}
-          {/*    <InputLabel>Shipping Country</InputLabel>*/}
-          {/*    <Select value={} fullWidth onChange={}>*/}
-          {/*      <MenuItem key={} value={}>*/}
-          {/*        Select Me*/}
-          {/*      </MenuItem>*/}
-          {/*    </Select>*/}
-          {/*  </Grid>*/}
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
+                {console.log(Object.entries(shippingCountries))}
+                {/*<MenuItem key={} value={}>*/}
+                {/*  Select Me*/}
+                {/*</MenuItem>*/}
+              </Select>
+            </Grid>
           {/*  <Grid item xs={12} sm={6}>*/}
           {/*    <InputLabel>Shipping Subdivision</InputLabel>*/}
           {/*    <Select value={} fullWidth onChange={}>*/}
